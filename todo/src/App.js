ef90605cbb0103be3204f356d368124647ffdfd2
VladislavMolodtsov <vladi28061750@gmail.com>
2023-05-27 18:27:44 +0300

    Lesson about props components

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,44 +1,22 @@
-import { Component, StrictMode } from 'react';
+
 import './App.css';
 
-const Header = () => {
-	return <h2>Hello World</h2>
-}
-
-function Btn() {
-	// const res = () => {
-	// 	return 'Log In';
-	// }
-	const text = 'Log In';
-	const logged = true;
-
-	return <button>{logged ? 'Enter' : text }</button>
-}
-
-class Field extends Component {
-	render() {
-		const holder = 'Enter Here';
-		const styleField = {
-			width: 300 + 'px'
-		}
-		return <input 
-					placeholder={holder} 
-					type='text' 
-					style={styleField}/>
-	}
+function WhoAmI({name, surname, link}) {
+	return (
+		<div>
+			<h1>My name is {name()}, surname - {surname}</h1>
+			<a href={link}>My profile</a>
+		</div>
+	)
 }
 
 function App() {
 	return (
 		<div className="App">
-			<StrictMode>
-				<Header/>
-			</StrictMode>
-			<Field/>
-			<Btn/>
+			<WhoAmI name={() => {return 'John'}} surname='Smith' link='facebook.com'/>
+			<WhoAmI name={() => {return 'John'}} surname='Smith' link='facebook.com'/>
 		</div>
-	);
+	)
 }
 
-export {Header};
 export default App;
